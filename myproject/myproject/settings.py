fd2420ababa8e08e373ae73fb111ed6a78482516
Ubuntu <ubuntu@ip-172-31-30-102.ec2.internal>
2025-03-14 18:00:04 +0000

    remove

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'drf_yasg',
     'rest_framework',
-    'rest_framework_simplejwt',
+   # 'rest_framework_simplejwt',
     'myapp',
     
 ]
